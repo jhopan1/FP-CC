75ce841df16f30abba24458a52db22923cf185da
jhopan1 <148057998+jhopan1@users.noreply.github.com>
2024-12-17 09:59:33 +0700

    Update koneksi.php

--- proses/koneksi.php
+++ proses/koneksi.php
@@ -1,3 +1,3 @@
 <?php
-    $koneksi = mysqli_connect("finalcc.mysql.database.azure.com", "finalcc", "Finalcc1", "akusewa", 3306, );
+    $koneksi = mysqli_connect("finalcc.mysql.database.azure.com", "finalcc", "Finalcc1", "sewa", 3306, );
 ?>
